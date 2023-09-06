0d17a67bcfdc5fc1cfa4bcfe428bb24683848a62
doanhtu07 <doanhtu07@gmail.com>
2023-09-06 15:14:25 -0500

    Finish adding Typescript, linter, formatter to client. Still need further refactor of legacy js files

--- client/Screens/LoginScreen.js
+++ client/Screens/LoginScreen.js
@@ -1,65 +1,84 @@
-import React from 'react';
-import { SafeAreaView, View, Text, Image } from 'react-native';
-import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
-import LoginSVG from '../assets/images/GDYO_Logo_Transparent.png';
-import CustomButton from '../components/CustomButton';
-import InputField from '../components/InputField';
+import React from "react";
+import { SafeAreaView, View, Text, Image } from "react-native";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
 
-const LoginScreen = ({navigation}) => {
+import LoginSVG from "@/assets/images/GDYO_Logo_Transparent.png";
+
+import CustomButton from "../components/CustomButton";
+import InputField from "../components/InputField";
+
+const LoginScreen = ({ navigation }) => {
   return (
-    <SafeAreaView style={{flex: 1, justifyContent: 'center', backgroundColor: '#10579B'}}>
-      <View style={{paddingHorizontal: 25}}>
-        <View style={{alignItems: 'center'}}>
-          <Image source={LoginSVG} style={{ height: 200, width: 200 }}/>  
+    <SafeAreaView
+      style={{ flex: 1, justifyContent: "center", backgroundColor: "#10579B" }}
+    >
+      <View style={{ paddingHorizontal: 25 }}>
+        <View style={{ alignItems: "center" }}>
+          <Image source={LoginSVG} style={{ height: 200, width: 200 }} />
         </View>
 
         <Text
           style={{
             fontSize: 28,
-            fontWeight: '500',
-            color: '#FFFFFF',
+            fontWeight: "500",
+            color: "#FFFFFF",
             marginTop: 20,
             marginBottom: 20,
-          }}>
+          }}
+        >
           Login
         </Text>
 
         <InputField
-          label={'Email'}
+          label={"Email"}
           icon={
             <MaterialIcons
               name="email"
               size={20}
               color="#FFFFFF"
-              style={{marginRight: 5, marginTop: 4}}
+              style={{ marginRight: 5, marginTop: 4 }}
             />
           }
           keyboardType="email-address"
         />
 
         <InputField
-          label={'Password'}
+          label={"Password"}
           icon={
             <MaterialIcons
               name="lock"
               size={20}
               color="#FFFFFF"
-              style={{marginRight: 5}}
+              style={{ marginRight: 5 }}
             />
           }
           inputType="password"
           fieldButtonLabel={"Forgot?"}
-          fieldButtonFunction={() => {}}
+          fieldButtonFunction={() => {
+            //
+          }}
         />
-          
-        <CustomButton label={"Login"} onPress={() => {navigation.navigate('TabNavigator')}} />
 
-        <Text style={{textAlign: 'center', color: '#FFFFFF', marginBottom: 20}}>
+        <CustomButton
+          label={"Login"}
+          onPress={() => {
+            navigation.navigate("TabNavigator");
+          }}
+        />
+
+        <Text
+          style={{ textAlign: "center", color: "#FFFFFF", marginBottom: 20 }}
+        >
           OR
         </Text>
 
         <View>
-          <CustomButton label={"Register"} onPress={() => {}} />
+          <CustomButton
+            label={"Register"}
+            onPress={() => {
+              //
+            }}
+          />
         </View>
       </View>
     </SafeAreaView>
