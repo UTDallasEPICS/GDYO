672023014d4ba191cbf5efe51b192eb6a0f01f26
doanhtu07 <doanhtu07@gmail.com>
2023-09-06 16:36:16 -0500

    Add alias support to babel

--- client/babel.config.js
+++ client/babel.config.js
@@ -1,6 +1,28 @@
+/* eslint-disable no-undef */
+
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "babel-plugin-root-import",
+        {
+          rootPathSuffix: "./",
+          rootPathPrefix: "@/",
+        },
+      ],
+    ],
+    env: {
+      production: {
+        plugins: [
+          "babel-plugin-root-import",
+          {
+            rootPathSuffix: "./",
+            rootPathPrefix: "@/",
+          },
+        ],
+      },
+    },
   };
 };
