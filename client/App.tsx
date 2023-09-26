ed0c6e5eb735361bb113d4e5880739c2de8fd567
doanhtu07 <doanhtu07@gmail.com>
2023-09-26 17:27:44 -0500

    Finish setting up Auth0 and test

--- client/App.tsx
+++ client/App.tsx
@@ -1,21 +1,27 @@
-import "expo-dev-client";
-
 // eslint-disable-next-line perfectionist/sort-imports
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Auth0Provider } from "react-native-auth0";
 
 import LoginScreen from "./Screens/LoginScreen";
 import TabNavigator from "./navigation/TabNavigator";
+
+import "expo-dev-client";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="LoginScreen" component={LoginScreen} />
-        <Stack.Screen name="TabNavigator" component={TabNavigator} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Auth0Provider
+      domain={"gdyo-auth.us.auth0.com"}
+      clientId={"umzG2Jd5sVtOKAHmySVQ49z3olmUYnD0"}
+    >
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="LoginScreen" component={LoginScreen} />
+          <Stack.Screen name="TabNavigator" component={TabNavigator} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Auth0Provider>
   );
 }
