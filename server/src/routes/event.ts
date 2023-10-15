6643613929011799dc762ed78d5fafbbff22dd94
AR0902 <abdurrahmanbaig0902@gmail.com>
2023-10-15 00:04:35 -0500

    Set up the API routes for event creation and filtered search of past events

--- server/src/routes/event.ts
+++ server/src/routes/event.ts
@@ -1,13 +1,49 @@
 import express from "express";
+import { report } from "@/utils/error";
+import context from "@/context";
 
 const router = express.Router();
 
-router.post("/add-single-event", (req, res) => {
-  // ...
+router.post("/add-single-event", async (req, res) => {
+  const { prisma } = context
+  try {
+    const eventData = req.body as {
+      name: string;
+      startTime: string;
+      endTime: string;
+      location: string;
+      description: string;
+    };
+    const createdEvent = await prisma.event.create({
+      data: {
+        name: eventData.name,
+        startTime: eventData.startTime,
+        endTime: eventData.endTime,
+        location: eventData.location,
+        description: eventData.description
+      }
+    });
+    res.json({ message: "Event added", event: createdEvent });
+  } catch (error) {
+    report(error);
+    res.status(500).json({ error: "internal Server error" });
+  }
 });
 
-router.get("/fetch-month-events", (req, res) => {
 
-});
+
+router.get("/fetch-month-events", async (req, res) => {
+  try {
+    const { dateTime } = req.query;
+    var date = dateTime.toString().split(' ');
+
+    const events = await prisma.event.findMany({
+      where: {
+        startTime: 
+      }
+
+
+    })
+  })
 
 export default router;
