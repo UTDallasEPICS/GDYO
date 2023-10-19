53e08b2fe7db0cfb1f67cc3d56a6976f6d53dae6
Jonathan Kopuri <kjernest01@gmail.com>
2023-10-19 17:54:46 -0500

    seeded db with 50 values

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -1,9 +1,21 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, Prisma } from "@prisma/client";
+
+import events from "./Event-data.json";
 
 const prisma = new PrismaClient();
+const data_size = 50;
 
 async function main() {
-  //
+  for (let i = 0; i < data_size; i++) {
+    const eventInput: Prisma.EventCreateInput = {
+      name: events[i].eventName,
+      startTime: new Date(`${events[i].dob} ${events[i].startTime}`),
+      endTime: new Date(`${events[i].dob} ${events[i].endTime}`),
+      location: `${events[i].location.street} ${events[i].location.town} ${events[i].location.postcode}}`,
+      description: events[i].description,
+    };
+    await prisma.event.create({ data: eventInput });
+  }
 }
 
 main()
