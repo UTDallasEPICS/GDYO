20c095b53be4b50f89a40d4690ff3e2e85bfde9b
Jonathan Kopuri <kjernest01@gmail.com>
2023-11-08 19:21:26 -0600

    added arrow for event list

--- client/pages/Home/EventManagementList-Home.tsx
+++ client/pages/Home/EventManagementList-Home.tsx
@@ -3,6 +3,7 @@ import ProfilePic from "assets/profile_pic.png";
 import react from "react";
 import { View, Image, Text, StyleSheet, Animated } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 import { CustomTheme, useCustomTheme } from "utils/theme";
 
 export const PROFILE_CONTAINER_HEIGHT = 260;
@@ -13,31 +14,24 @@ export const EventManagementList = () => {
   const theme = useCustomTheme();
   const styles = makeStyles(theme);
   const insets = useSafeAreaInsets();
-
+  // get number of events
   return (
-    // <View>
     <View style={styles.eventContainer}>
-      <View style={styles.eventColor}>
-        <View style={styles.MonthBox}>
-          <Text>Dec</Text>
-          <Text style={[{ fontSize: 40 }, { lineHeight: 43 }]}>30</Text>
-        </View>
-        <View style={styles.EventDetailBox}>
-          <Text
-            style={[
-              styles.TextBase,
-
-              { fontWeight: "normal" },
-              { fontSize: 13 },
-            ]}
-          >
-            8:00 PM
-          </Text>
-          <Text style={[styles.TextBase, styles.TextContainer]}>
-            GDYO Season Opener
-          </Text>
-          <Text style={styles.TextBase}>Moody Performance Hall</Text>
-        </View>
+      <View style={styles.eventArrow}></View>
+      <View style={styles.MonthBox}>
+        <Text>Dec</Text>
+        <Text style={[{ fontSize: 40 }, { lineHeight: 43 }]}>30</Text>
+      </View>
+      <View style={styles.EventDetailBox}>
+        <Text
+          style={[styles.TextBase, { fontWeight: "normal" }, { fontSize: 13 }]}
+        >
+          8:00 PM
+        </Text>
+        <Text style={[styles.TextBase, { fontSize: 15 }]}>
+          GDYO Season Opener
+        </Text>
+        <Text style={styles.TextBase}>Moody Performance Hall</Text>
       </View>
     </View>
   );
@@ -46,44 +40,37 @@ export const EventManagementList = () => {
 //helper functions(like styling or whatever).
 const makeStyles = (theme: CustomTheme) =>
   StyleSheet.create({
-    eventColor: {
+    eventContainer: {
       flexDirection: "row",
       display: "flex",
-      marginLeft: "10%",
-      height: "100%",
-      backgroundColor: theme.colors.primary,
-    },
-    eventContainer: {
       marginTop: "4%",
       height: PROFILE_CONTAINER_HEIGHT * 0.3,
       width: "100%",
-      backgroundColor: "red",
-      // width: 0,
-      // height: 0,
-      // borderTop: 25px solid transparent,
-      // borderLeft: 50px solid #555,
-      // border-bottom: 25px solid transparent,
-    },
-    TextBase: {
-      fontWeight: "bold",
-      fontSize: 12,
-    },
-    TextContainer: {
-      // paddingTop: 30,
-
-      fontSize: 15,
+      backgroundColor: theme.colors.primary,
     },
     MonthBox: {
+      // flex: 1,
       height: "100%",
       width: "25%",
-      //paddingLeft: 20,
       alignItems: "center",
       justifyContent: "center",
     },
     EventDetailBox: {
+      // flex: 2,
       paddingTop: 10,
-
       height: "100%",
       width: "100%",
+    },
+    eventArrow: {
+      borderTopWidth: 38,
+      borderTopColor: "transparent",
+      borderLeftWidth: 40,
+      borderLeftColor: "red",
+      borderBottomWidth: 38,
+      borderBottomColor: "transparent",
+    },
+    TextBase: {
+      fontWeight: "bold",
+      fontSize: 12,
     },
   });
