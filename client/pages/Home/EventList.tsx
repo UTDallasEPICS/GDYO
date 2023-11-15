bd65c060bff5773092c3c6269baa3039409a7ea9
Jonathan Kopuri <kjernest01@gmail.com>
2023-11-15 16:52:36 -0600

    added more events to homepage

--- client/pages/Home/EventList.tsx
+++ client/pages/Home/EventList.tsx
@@ -11,7 +11,7 @@ export const EventList = () => {
         time={"8:00 AM"}
         name={"GDYO Season Opener"}
         place={"Moody Orchestra Hall"}
-        color={"red"}
+        color={"rgba(0,179,131,1)"}
       ></EventItem>
       <EventItem
         date={1}
@@ -19,7 +19,7 @@ export const EventList = () => {
         time={"12:00 PM"}
         name={"Piano Heaven"}
         place={"Collin, TX"}
-        color={"green"}
+        color={"rgba(157,26,26,1)"}
       ></EventItem>
       <EventItem
         date={15}
@@ -27,15 +27,31 @@ export const EventList = () => {
         time={"10:30 AM"}
         name={"Big Orchestra"}
         place={"Richardson, TX"}
-        color={"yellow"}
+        color={"rgba(58,164,200,1)"}
       ></EventItem>
       <EventItem
         date={30}
         month={"Feb"}
         time={"7:00 PM"}
         name={"GDYO Fall"}
+        place={"Chennai, MA"}
+        color={"rgba(146,58,200,1)"}
+      ></EventItem>
+      <EventItem
+        date={9}
+        month={"March"}
+        time={"7:00 PM"}
+        name={"GDYO Fall"}
         place={"Denver, CO"}
-        color={"blue"}
+        color={"rgba(255,0,0,1)"}
+      ></EventItem>
+      <EventItem
+        date={31}
+        month={"March"}
+        time={"7:00 PM"}
+        name={"GDYO Fall"}
+        place={"Ohio, MI"}
+        color={"rgba(255,140,25,1)"}
       ></EventItem>
     </View>
   );
